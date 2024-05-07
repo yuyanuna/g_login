ca72683a4a62c610eb90239a02e6ee1c1cee39ab
yuyanuna <115072871+yuyanuna@users.noreply.github.com>
2024-05-07 22:04:00 +0800

    Initial commit

---
+++ g.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // 在這裡添加登入邏輯
+    console.log("Email: " + email + ", Password: " + password);
+    // 在這裡可以向後端發送登入請求，進行驗證等操作
+});
